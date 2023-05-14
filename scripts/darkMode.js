f0d252fad03208d389d3d6f73dd683d1b4d3b71d
Cayden <114540789+caydenpark@users.noreply.github.com>
2023-05-13 20:43:31 -0600

    W02 Project Milestone

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,14 +1,32 @@
-const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
 
-modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("☑️")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
-		modeButton.textContent = "❎";
+const darkBtn = document.querySelector("#dark");
+const main = document.querySelector("main")
+const h3 = document.querySelectorAll("h3")
+const card = document.querySelectorAll(".card")
+
+h3.forEach(function(e) {
+	e.style.color = 'black'
+})
+
+darkBtn.addEventListener("click", function() {
+
+	if (darkBtn.textContent.includes("⚫️")) {
+		// Make all card borders white
+		card.forEach(function(e) {
+			e.style.border = "1px solid white"
+		})
+		main.style.backgroundColor = "black"
+		main.style.color = "white"
+		darkBtn.textContent = "⚪️"
+		darkBtn.classList.toggle('darkMode');
 	} else {
-		main.style.background = "#eee";
-		main.style.color = "#000";
-		modeButton.textContent = "☑️";
+		// Make all card borders black
+		card.forEach(function(e) {
+			e.style.border = "1px solid black"
+		})
+		main.style.color = "black"
+		main.style.backgroundColor = "white"
+		darkBtn.textContent = "⚫️"
+		darkBtn.classList.toggle('darkMode');
 	}
-});
+})
